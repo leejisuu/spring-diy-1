a4c4b52ee6ae542a99d55c572ac908e12fb35ab4
leejisuu <dlwltn966@gmail.com>
2026-04-29 02:24:36 +0900

    [feat] DispatcherServlet, Controller, ModelAndView 구현

---
+++ src/main/java/com/diy/app/lecture/LectureController.java
@@ -0,0 +1,52 @@
+package com.diy.app.lecture;
+
+import com.diy.framework.web.render.ModelAndView;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LectureController implements Controller{
+    private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private final LectureRepository lectureRepository = new LectureRepository();
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String method = request.getMethod();
+
+        return switch (method) {
+            case "GET" -> doGet(request, response);
+            case "POST" -> doPost(request, response);
+            case "PUT" -> doPut(request, response);
+            case "DELETE" -> doDelete(request, response);
+            default -> throw new RuntimeException("지원하지 않는 HTTP 메서드입니다");
+        };
+    }
+
+    protected ModelAndView doGet(HttpServletRequest req, HttpServletResponse resp) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("lectures", lectureRepository.findAll());
+        return new ModelAndView("lecture-list", model);
+    }
+
+    protected ModelAndView doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Lecture lecture = OBJECT_MAPPER.readValue(req.getInputStream(), Lecture.class);
+        lectureRepository.insert(lecture);
+        return new ModelAndView("redirect:/lectures");
+    }
+
+    protected ModelAndView doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Lecture lecture = OBJECT_MAPPER.readValue(req.getInputStream(), Lecture.class);
+        lectureRepository.update(lecture);
+        return new ModelAndView("redirect:/lectures");
+    }
+
+    protected ModelAndView doDelete(HttpServletRequest req, HttpServletResponse resp) {
+        final Long id = Long.valueOf(req.getParameter("id"));
+        lectureRepository.delete(id);
+        return new ModelAndView("redirect:/lectures");
+    }
+}
---
+++ src/main/java/com/diy/framework/web/DispatcherServlet.java
@@ -0,0 +1,62 @@
+package com.diy.framework.web;
+
+import com.diy.app.lecture.Controller;
+import com.diy.app.lecture.LectureController;
+import com.diy.framework.web.render.ModelAndView;
+import com.diy.framework.web.render.view.View;
+import com.diy.framework.web.render.resolver.ViewResolver;
+import com.diy.framework.web.render.resolver.ViewResolverFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    private final Map<String, Controller> controllersMapping = new HashMap<>();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        controllersMapping.put("/lectures", new LectureController());
+    }
+
+    @Override
+    protected void service(final HttpServletRequest req, final HttpServletResponse resp) {
+        Controller controller = controllersMapping.get(req.getServletPath());
+        if(controller == null) return;
+
+        try {
+            final ModelAndView mav = controller.handleRequest(req, resp);
+
+            ViewResolver viewResolver = ViewResolverFactory.getViewResolver(mav.getViewName());
+
+            render(viewResolver, mav, req, resp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void render(final ViewResolver viewResolver,
+                        final ModelAndView mav,
+                        final HttpServletRequest req,
+                        final HttpServletResponse resp) throws Exception {
+        final String viewName = mav.getViewName();
+
+        final View view = viewResolver.resolveViewName(viewName);
+
+        if (view == null) {
+            throw new RuntimeException("View not found: " + viewName);
+        }
+
+        view.render(mav.getModel(), req, resp);
+    }
+}
+
