2c103504996a628f27ced1ad2842cc3f95be19cd
leejisuu <dlwltn966@gmail.com>
2026-05-07 17:23:29 +0900

    feat: 메인 패키지에서 bean 스캔하도록 추가

--- src/main/java/com/diy/app/LectureApplication.java
+++ src/main/java/com/diy/app/LectureApplication.java
@@ -1,9 +1,13 @@
 package com.diy.app;
 
+import com.diy.framework.context.ApplicationContext;
 import com.diy.framework.web.server.TomcatWebServer;
 
 public class LectureApplication {
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ApplicationContext(LectureApplication.class.getPackageName());
+        applicationContext.initialize();
+
         TomcatWebServer tomcatWebServer = new TomcatWebServer();
         tomcatWebServer.start();
     }
